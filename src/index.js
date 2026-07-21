d981c14560c495480e2d935b95ae118d20ab5653
BimbDevp <BimbDevp@users.noreply.github.com>
2026-07-21 14:46:24 +0700

    Update dist

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import "./style.css";
-import { initApp, bindEvents} from "./controller/controller.js";
-
-
-
-initApp();
-bindEvents();
--- src/model/project.js
+++
@@ -1,8 +0,0 @@
-export default function createProject(title){
-    return {
-        id: crypto.randomUUID(),
-        title: title,
-        createdAt: new Date(),
-        isDefault: false,
-    };
-}
